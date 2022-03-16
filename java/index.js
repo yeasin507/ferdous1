83ce07a28ae3aab5e69ae368369abcacaf22da40
yeasin507 <101644122+yeasin507@users.noreply.github.com>
2022-03-17 01:42:41 +0600

    Add files via upload

--- java/index.js
+++ java/index.js
@@ -1,7 +1,8 @@
 var hamburger = document.querySelector(".hamburger");
+var hamburgerMenu = document.querySelector(".hamburger_menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
+    hamburgerMenu.classList.toggle("header_menu");
 }
-
